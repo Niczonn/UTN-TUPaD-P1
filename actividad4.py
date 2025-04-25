ae60bee835b7ccde414442b7b1534e873b23aeb2
NicolasFeldmann(Niczonn) <90484395+Niczonn@users.noreply.github.com>
2025-04-25 17:49:30 -0300

    Delete actividad4.py

--- actividad4.py
+++
@@ -1,5 +0,0 @@
-animales = ["perro", "gato", "conejo", "pez"]
-animales[1] = "loro"  # Reemplazamos el segundo valor (índice 1)
-animales[-1] = "oso"  # Reemplazamos el último valor con índice negativo -1
-print(animales)
-
